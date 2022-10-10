d0752875a00386d8b69f9550167e63c35fe6d38b
Dinh Van Duc <dinhduc2550@gmail.com>
2022-10-10 19:55:08 +0700

    update

--- home-sharing/src/app/app.module.ts
+++ home-sharing/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { HomeComponent } from './home/home.component';
 import {AuthService} from "./auth/auth.service";
 import {AppRoutingModule} from "./app-routing.module";
 import {HttpClientModule} from "@angular/common/http";
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
     HomeComponent,
+    HeaderComponent,
 
   ],
   imports: [
