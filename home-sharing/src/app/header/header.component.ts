6f2e156203b39f5a80e39f7b1b5b0e639da5c29c
khaitqhe141672 <65528932+khaitqhe141672@users.noreply.github.com>
2022-10-11 06:13:27 +0700

    update html css homepage

--- home-sharing/src/app/header/header.component.ts
+++ home-sharing/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-header',
@@ -10,6 +11,20 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(){
+    $(document).ready(function () {
+      $(".dropdown").click(function () {
+        $(this).find(".dropdown-content").slideToggle("fast");
+      });
+    });
+    $(document).on("click", function (event) {
+      var $trigger = $(".dropdown");
+      if ($trigger !== event.target && !$trigger.has(event.target).length) {
+        $(".dropdown-content").slideUp("fast");
+      }
+    });
   }
 
 }
