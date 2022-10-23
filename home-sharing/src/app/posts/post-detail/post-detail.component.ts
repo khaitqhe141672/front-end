4d5db5cf3da37cd4b0e2db4c4a7a559347e9f5e9
San <dinhduc2550@gmail.com>
2022-10-23 21:40:36 +0700

    update post detail

--- home-sharing/src/app/posts/post-detail/post-detail.component.ts
+++ home-sharing/src/app/posts/post-detail/post-detail.component.ts
@@ -5,6 +5,7 @@ import {Post, ReponsePost} from "../post.model";
 import {Subscription} from "rxjs";
 import {DataStorageService} from "../../shared/data-storage.service";
 import {PostDetailService} from "./post-detail.service";
+import {Rate, RateResponse} from "../../shared/model/rate.model";
 
 @Component({
   selector: 'app-post-detail',
@@ -12,9 +13,11 @@ import {PostDetailService} from "./post-detail.service";
   styleUrls: ['./post-detail.component.css']
 })
 export class PostDetailComponent implements OnInit {
-  id
-  postDetail:Post
-  postDetailResponse:ReponsePost
+  id:number;
+  postDetail:Post;
+  postDetailResponse:ReponsePost;
+  rateResponse:RateResponse
+  rates:Rate[] = []
   constructor(private postService:PostService,private router:Router,private route:ActivatedRoute, private dataStorage:DataStorageService,private postDetailService:PostDetailService) { }
 
   ngOnInit(): void {
@@ -22,5 +25,20 @@ export class PostDetailComponent implements OnInit {
       this.id = +params['id']
       this.postDetail = this.postDetailService.postDetail
     })
+    this.getRate()
+  }
+  getRate(){
+    this.postDetailService.getRatesByPostID(this.id).subscribe(
+      responseRate =>{
+        this.rateResponse = responseRate
+        this.rates = responseRate.object
+        console.log(this.rates[0])
+
+      },
+      error => console.log(error)
+    )
+  }
+  counterRate(i: number){
+    return new Array(i)
   }
 }
--- home-sharing/src/app/posts/post-detail/post-detail.service.ts
+++ home-sharing/src/app/posts/post-detail/post-detail.service.ts
@@ -1,13 +1,24 @@
 import {Injectable} from "@angular/core";
 import {Post} from "../post.model";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {RateResponse} from "../../shared/model/rate.model";
+import {API_RATE} from "../../constant/api.constant";
 
 @Injectable({providedIn:'root'})
 export class PostDetailService{
+  constructor(private http:HttpClient) {
+  }
     postDetail:Post
   bindPostData(postDetail:Post){
       this.postDetail = postDetail
   }
   get ResponsePostData(){
       return this.postDetail
+  }
+
+  getRatesByPostID(id:number):Observable<RateResponse>
+  {
+      return this.http.get<RateResponse>(API_RATE+id)
   }
 }
