e7c84e79d211f83029c70bc564fa6b4e36ab9e6d
khaitqhe141672 <65528932+khaitqhe141672@users.noreply.github.com>
2022-11-06 23:14:09 +0700

    Update test.service.ts

--- home-sharing/src/app/test/test.service.ts
+++ home-sharing/src/app/test/test.service.ts
@@ -3,22 +3,22 @@ import {HttpClient, HttpEvent, HttpHeaders, HttpRequest} from "@angular/common/h
 import {Observable} from "rxjs";
 import {Injectable} from "@angular/core";
 import {H} from "@angular/cdk/keycodes";
-@Injectable({providedIn:'root'})
-export class TestService{
-  private baseUrl = API_PUSH_SINGLE_IMG_POST+3;
 
-  constructor(private http: HttpClient) { }
+@Injectable({providedIn: 'root'})
+export class TestService {
+  private baseUrl = API_PUSH_SINGLE_IMG_POST + 3;
+
+  constructor(private http: HttpClient) {
+  }
+
   // headerOption = new HttpHeaders()
-  baseApiUrl = 'http://localhost:8080/api/posting/insert-post-image-one?post-id=3';
-
-
+  baseApiUrl = 'http://localhost:8080/api/posting/insert-post-image-one?post-id=6';
 
 
   upload(file: File): Observable<any> {
     const formData: FormData = new FormData();
     // this.headerOption.append('Content-Type','multipart/form-data')
-    formData.append('file', file,file.name);
-
+    formData.append('file', file, file.name);
 
 
     const req = new HttpRequest('POST', `${this.baseApiUrl}`, formData, {
@@ -30,8 +30,8 @@ export class TestService{
 
     return this.http.request(req);
 
-  const headers = new HttpHeaders({"Content-Type":"multipart/form-data",'Test-xem':'ahihi'});
-    return this.http.post(this.baseApiUrl,formData, {headers : new HttpHeaders({ 'Content-Type':'multipart/form-data'})});
+    const headers = new HttpHeaders({"Content-Type": "multipart/form-data", 'Test-xem': 'ahihi'});
+    return this.http.post(this.baseApiUrl, formData, {headers: new HttpHeaders({'Content-Type': 'multipart/form-data'})});
 
   }
 
